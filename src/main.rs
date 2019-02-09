2686cead69a095a671ffade919831f675833557e
funemy <liyzunique@gmail.com>
2019-02-08 20:56:18 -0600

    add println

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn panic(_info: &PanicInfo) -> ! {
 // linux start
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numers: {} {}", 42, 1.337).unwrap();
+
+    println!("Hello World{}", "!");
+
     loop {}
 }
